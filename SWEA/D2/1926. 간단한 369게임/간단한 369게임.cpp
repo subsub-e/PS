174dcaeff8ff5872d9cb558be7e9126414407d23
subsub-e <55149395+subsub-e@users.noreply.github.com>
2024-04-17 23:28:19 +0900

    [D2] Title: 간단한 369게임, Time: 6 ms, Memory: 13,380 KB -BaekjoonHub

---
+++ SWEA/D2/1926. 간단한 369게임/간단한 369게임.cpp
@@ -0,0 +1,57 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// float b, c;
+// double d, e, f;
+// char g;
+// char var[256];
+// long long AB;
+// cin >> a;                            // int 변수 1개 입력받는 예제
+// cin >> b >> c;                       // float 변수 2개 입력받는 예제 
+// cin >> d >> e >> f;                  // double 변수 3개 입력받는 예제
+// cin >> g;                            // char 변수 1개 입력받는 예제
+// cin >> var;                          // 문자열 1개 입력받는 예제
+// cin >> AB;                           // long long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// float b = 1.0, c = 2.0;               
+// double d = 3.0, e = 0.0; f = 1.0;
+// char g = 'b';
+// char var[256] = "ABCDEFG";
+// long long AB = 12345678901234567L;
+// cout << a;                           // int 변수 1개 출력하는 예제
+// cout << b << " " << c;               // float 변수 2개 출력하는 예제
+// cout << d << " " << e << " " << f;   // double 변수 3개 출력하는 예제
+// cout << g;                           // char 변수 1개 출력하는 예제
+// cout << var;                         // 문자열 1개 출력하는 예제
+// cout << AB;                          // long long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int n; cin >> n;
+    for(int i = 1; i <= n; i++){
+    	string temp = to_string(i);
+        int flag = 0;
+        for(int j = 0; j < temp.length(); j++){
+        	if(temp[j] == '3' || temp[j] == '6' || temp[j] == '9'){
+            	flag = 1;
+                cout << '-';
+            }
+        }
+        if(flag == 1){
+            cout << ' ';
+        }
+        else{
+            cout << temp << ' ';
+        }
+    }
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
