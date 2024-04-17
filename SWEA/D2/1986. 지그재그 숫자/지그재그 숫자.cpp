e8180cab5d405d2dd216d38f599a66eb8e9ec2db
subsub-e <55149395+subsub-e@users.noreply.github.com>
2024-04-18 00:06:06 +0900

    [D2] Title: 지그재그 숫자, Time: 8 ms, Memory: 13,372 KB -BaekjoonHub

---
+++ SWEA/D2/1986. 지그재그 숫자/지그재그 숫자.cpp
@@ -0,0 +1,29 @@
+
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+	cin>>T;
+	/*
+	   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+	*/
+	for(test_case = 1; test_case <= T; ++test_case)
+	{
+		int n; cin >> n;
+        int sum = 0;
+        for(int i = 1; i <= n; i++){
+        	if(i % 2 == 1){
+            	sum += i;
+            }
+            else{
+                sum -= i;
+            }
+        }
+        cout << '#' << test_case << ' ' << sum << '\n';
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
