50eac0c6dc2387af730f1f97c1130a9499949a45
subsub-e <55149395+subsub-e@users.noreply.github.com>
2024-06-19 19:20:12 +0900

    [level 0] Title: 약수 구하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    var answer = [];
+    for(let i = 1; i <= n; i++){
+        if(n % i === 0){
+            answer.push(i);
+        }
+    }
+    answer.sort((a, b) => a - b);
+    return answer;
+}
