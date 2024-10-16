283fdb767330282808c85d6e3904b43abe601015
subsub-e <55149395+subsub-e@users.noreply.github.com>
2024-10-16 14:24:06 +0900

    [level 2] Title: 전화번호 목록, Time: 639.15 ms, Memory: 56.8 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
 
 ### 성능 요약
 
-메모리: 49.4 MB, 시간: 292.37 ms
+메모리: 56.8 MB, 시간: 639.15 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 07일 03:03:23
+2024년 10월 16일 14:24:04
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -1,31 +1,29 @@
 #include <string>
 #include <vector>
-#include <unordered_set>
-#include <climits>
 #include <algorithm>
+#include <unordered_map>
 #include <iostream>
+#include <string>
+#include <set>
 using namespace std;
+
 
 bool solution(vector<string> phone_book) {
     bool answer = true;
-    unordered_set<string> s;
+    set<string> s;
+    unordered_map<int, string> m;
     
     for(int i = 0; i < phone_book.size(); i++){
         s.insert(phone_book[i]);
     }
     
     for(int i = 0; i < phone_book.size(); i++){
-        string temp = "";
-        for(int j = 0; j < phone_book[i].length(); j++){
-            temp += phone_book[i][j];
-            if(j == phone_book[i].length() - 1){
-                break;
-            }
-            if(s.find(temp) != s.end()){
-                answer = false;
+        for(int j = 1; j < phone_book[i].length(); j++){
+            if(s.find(phone_book[i].substr(0, j)) != s.end()){
+                // cout << phone_book[i].substr(0, j) << '\n';
+                return false;
             }
         }
     }
-    
-     return answer;
+    return answer;
 }
