d1e5cf124d4e5a88418b8fbc9e5dfcc508f70b15
이승섭 <55149395+subsub-e@users.noreply.github.com>
2025-01-08 19:00:20 +0900

    [Gold IV] Title: 타임머신, Time: 12 ms, Memory: 2416 KB -BaekjoonHub

---
+++ 백준/Gold/11657. 타임머신/타임머신.cc
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+int n, m;
+long long dist[510];
+vector<pair<int, pair<int, long long> > > v;
+const long long INF = 987654321;
+
+int main(){
+    cin >> n >> m;
+    fill(dist + 1, dist + 1 + n, INF);
+
+    for(int i = 0; i < m; i++){
+        int from, to, d;
+        cin >> from >> to >> d;
+        v.push_back(make_pair(from, make_pair(to, d)));
+    }
+
+    dist[1] = 0;
+
+    for(int i = 1; i <= n - 1; i++){
+        for(int j = 0; j < v.size(); j++){
+            int from = v[j].first;
+            int to = v[j].second.first;
+            long long cost = v[j].second.second;
+
+            if(dist[from] == INF){
+                continue;
+            }
+            if(dist[to] > dist[from] + cost){
+                dist[to] = dist[from] + cost;
+            }
+        }
+    }
+
+    for(int i = 0; i < v.size(); i++){
+        int from = v[i].first;
+        int to = v[i].second.first;
+        long long cost = v[i].second.second;
+        if(dist[from] == INF){
+            continue;
+        }
+        if(dist[to] > dist[from] + cost){
+            cout << -1;
+            return 0;
+        }
+    }
+
+    for(int i = 2; i <= n; i++){
+        if(dist[i] == INF){
+            cout << -1 << '\n';
+        }
+        else{
+            cout << dist[i] << '\n';
+        }
+    }
+}
