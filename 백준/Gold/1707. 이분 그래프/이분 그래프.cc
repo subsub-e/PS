1860a23d2594c16cd80161afdefa24bab3ec489c
이승섭 <55149395+subsub-e@users.noreply.github.com>
2025-03-06 23:33:26 +0900

    [Gold IV] Title: 이분 그래프, Time: 192 ms, Memory: 11568 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.cc
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.cc
@@ -1,90 +1,94 @@
 #include <iostream>
-#include <queue>
-#include <algorithm>
-#include <functional>
 #include <vector>
-#include <climits>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <cstring>
 
 using namespace std;
 
-int t;
-int n, m;
-vector<int> v[20001];
+int k, v, e;
+vector<int> t[20001];
 bool visited[20001];
 vector<pair<int, int> > ans;
 
 void bfs(int x){
-    queue<pair<int,int> > q;
-    int red = 1;
-    q.push(make_pair(x, red));
     visited[x] = 1;
+    queue<pair<int, int> > q;
+    q.push({x, 0});
 
     while(!q.empty()){
         int now = q.front().first;
         int color = q.front().second;
-        ans.push_back(make_pair(now, color % 2));
+        ans.push_back({now, color % 2});
         q.pop();
 
-        for(auto next : v[now]){
+        for(auto next : t[now]){
             if(!visited[next]){
                 visited[next] = 1;
-                q.push(make_pair(next, color + 1));
+                q.push({next, color + 1});
             }
         }
     }
-    return;
 }
 
 bool func(){
-    for(int i = 1; i <= n; i++){
-        for(int j = 0; j < (int) v[i].size(); j++){
-            int next = v[i][j];
-            if(ans[i - 1].second == ans[next - 1].second){
-                return 0;
+    for(int i = 1; i <= v; i++){
+        for(auto next : t[i]){
+            if(ans[i-1].second == ans[next-1].second){
+                return false;
             }
         }
     }
-    return 1;
+    return true;
 }
 
-int main(void) {
-    cin.tie(0);
-    ios_base::sync_with_stdio(false);
-	
-    
-    cin >> t;
-    for(int i = 0; i < t; i++){
-        
-        cin >> n >> m;
-        for(int j = 0; j <= n; j++){
-            visited[j] = 0;
-        }
-        
-        for(int j = 0; j < m; j++){
-            int x1, x2; cin >> x1 >> x2;
-            v[x1].push_back(x2);
-            v[x2].push_back(x1);
-        }
+void solution(){
+    for(int i = 1; i <= v; i++){
+        t[i].clear();
+    }
+    ans.clear();
+    memset(visited, 0, sizeof(visited));
 
-        for(int j = 1; j <= n; j++){
-            if(!visited[j]){
-                bfs(j);
-            }
-        }
 
-        sort(ans.begin(), ans.end());
+    for(int i = 0; i < e; i++){
+        int x, y;
+        cin >> x >> y;
+        t[x].push_back(y);
+        t[y].push_back(x);
+    }
 
-        if(func()){
-            cout << "YES" << '\n';
-        }
-        else{
-            cout << "NO" << '\n';
-        }
-
-        ans.clear();
-        for(int j = 0; j <= n; j++){
-            v[j].clear();
+    for(int i = 1; i <= v; i++){
+        if(!visited[i]){
+            bfs(i);
         }
     }
+
+    // for(int i = 0; i < ans.size(); i++){
+    //     cout << ans[i].first << ' ' << ans[i].second << '\n';
+    // }
+
+    sort(ans.begin(), ans.end());
+
+    if(func()){
+        cout << "YES" << '\n';
+    }
+    else{
+        cout << "NO" << '\n';
+    }
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> k;
+    while(k--){
+        cin >> v >> e;
+        solution();
+    }
+
     return 0;
 }
