ff42664b1b8957ea9db70bcf80b39ff228b3c2d1
이승섭 <55149395+subsub-e@users.noreply.github.com>
2025-03-07 19:20:29 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 804 ms, Memory: 70644 KB -BaekjoonHub

--- 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.cc
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.cc
@@ -1,42 +1,57 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
-
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <cstring>
+#include <set>
+#include <tuple>
+#include <cmath>
 using namespace std;
-
 
 int n;
 vector<int> v[1000001];
-int dp[1000001][2];
 bool visited[1000001];
+int dp[1000001][2];
+const int INF = 1e9 + 10;
 
-void find(int now){
+void dfs(int now){
     visited[now] = 1;
+    dp[now][0] = 0;
     dp[now][1] = 1;
+
     for(auto next : v[now]){
         if(!visited[next]){
-            find(next);
-            dp[now][1] += min(dp[next][1], dp[next][0]);
-            dp[now][0] += dp[next][1]; 
+            visited[next] = 1;
+            dfs(next);
+
+            dp[now][0] += dp[next][1];
+            dp[now][1] += min(dp[next][0], dp[next][1]);
         }
     }
 }
 
-int main(){
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
     cin >> n;
-    // int maxans = 0;
-    for(int i = 0; i < n-1; i++){
-        int st, en;
-        cin >> st >> en;
-        // maxans = max(maxans, st);
-        // maxans = max(maxans, en);
-        v[st].push_back(en);
-        v[en].push_back(st);
+
+    for(int i = 0; i <= n; i++){
+        dp[i][0] = INF;
+        dp[i][1] = INF;
+    }
+    for(int i = 0; i < n; i++){
+        int x, y;
+        cin >> x >> y;
+        v[x].push_back(y);
+        v[y].push_back(x);
     }
 
-    find(1);
-    // for(int i = 1; i <= maxans; i++){
-    //     cout << dp[i][0] << ' ' << dp[i][1] << '\n';
-    // }
+    dfs(1);
+
     cout << min(dp[1][0], dp[1][1]);
+
+    return 0;
 }
