ea5716000af668761301ea58daeadd6cdb847dd9
이승섭 <55149395+subsub-e@users.noreply.github.com>
2025-03-07 17:41:30 +0900

    [Gold IV] Title: 여행 가자, Time: 0 ms, Memory: 5940 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.cc
+++ 백준/Gold/1976. 여행 가자/여행 가자.cc
@@ -1,52 +1,73 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <string>
+#include <queue>
 #include <cstring>
-
+#include <set>
+#include <tuple>
 using namespace std;
 
-
+int uf[201];
 int n, m;
-int arr[201][201];
-const int INF = 1e9 + 10;
+int arr[1001][1001];
+vector<int> v;
 
-int main(){
+int find(int x){
+    if(uf[x] == x){
+        return x;
+    }
+    return uf[x] = find(uf[x]);
+}
+
+void Merge(int x, int y){
+    int X = find(x);
+    int Y = find(y);
+    if(X == Y){
+        return;
+    }
+    else{
+        uf[X] = Y;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
     cin >> n >> m;
+    for(int i = i; i <= n; i++){
+        uf[i] = i;
+    }
 
     for(int i = 1; i <= n; i++){
         for(int j = 1; j <= n; j++){
             cin >> arr[i][j];
-            if(arr[i][j] == 0){
-                arr[i][j] = INF;
-            }
         }
     }
-    
+
     for(int i = 1; i <= n; i++){
-        arr[i][i] = 0;
-    }
-
-    for(int k = 1; k <= n; k++){
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= n; j++){
-                arr[i][j] = min(arr[i][j], arr[i][k]+ arr[k][j]);
+        for(int j = i + 1; j <= n; j++){
+            if(arr[i][j] == 1){
+                if(find(i) != find(j)){
+                    Merge(i, j);
+                }
             }
         }
     }
 
-    vector<int> v;
     for(int i = 0; i < m; i++){
         int x; cin >> x;
         v.push_back(x);
     }
 
-    for(int i = 1; i < m; i++){
-        if(arr[v[i-1]][v[i]] == INF){
-            cout << "NO";
+    for(int i = 1; i < v.size(); i++){
+        if(find(v[i]) != find(v[i-1])){
+            cout << "NO" << '\n';
             return 0;
         }
-    }   
-
-    cout << "YES";
-
+    }
+    cout << "YES" << '\n';
+    return 0;
 }
